f4ae5770cb878ab091bd90083691354ff90872c6
ndrewA <andrewbach1999@gmail.com>
2023-11-01 22:07:28 +0200

    added minor keymapping updates

--- lua/user/keymappings.lua
+++ lua/user/keymappings.lua
@@ -7,17 +7,11 @@ local keymap = vim.api.nvim_set_keymap
 -- explorer
 keymap('n', '<leader>e', ':Ex<CR>', opts)
 
--- navigate windows
-keymap('n', 'gh', '<C-w>h', opts)
-keymap('n', 'gj', '<C-w>j', opts)
-keymap('n', 'gk', '<C-w>k', opts)
-keymap('n', 'gl', '<C-w>l', opts)
-
 -- navigate terminals
-keymap('t', 'gh', "<C-\\><C-N><C-w>h", opts)
-keymap('t', 'gj', "<C-\\><C-N><C-w>j", opts)
-keymap('t', 'gk', "<C-\\><C-N><C-w>k", opts)
-keymap('t', 'gl', "<C-\\><C-N><C-w>l", opts)
+keymap('t', '<C-w>h', "<C-\\><C-N><C-w>h", opts)
+keymap('t', '<C-w>j', "<C-\\><C-N><C-w>j", opts)
+keymap('t', '<C-w>k', "<C-\\><C-N><C-w>k", opts)
+keymap('t', '<C-w>l', "<C-\\><C-N><C-w>l", opts)
 
 -- split
 keymap('n', '<leader>hts', ":split | terminal<CR>", opts) -- horizontal terminal
@@ -30,8 +24,13 @@ keymap('n', '<leader>he', ":Sexplore<CR>", opts) -- horizontal explorer
 keymap('n', '<leader>ve', ":Vexplore<CR>", opts) -- vertical explorer
 
 -- navigate buffers
-keymap('n', '<C-l>', ':bnext<CR>', opts)
-keymap('n', '<C-h>', ':bprevious<CR>', opts)
+keymap('n', '<Tab>', ':bnext<CR>', opts)
+keymap('n', '<S-Tab>', ':bprevious<CR>', opts)
+
+-- close all buffers except the one in focus
+keymap('n', '<C-c>b',
+       ':silent let save_pos = getpos(".") | silent wa | silent %bd | silent edit# | silent bd# | silent call setpos(".", save_pos)<CR>',
+       opts)
 
 -- resize window
 keymap('n', '<C-Up>', ':resize +2<CR>', opts)
@@ -53,10 +52,48 @@ keymap("n", "<A-k>", ":m .-2<CR>==", opts)
 -- dont yank when pasting
 keymap('v', 'p', '"_dP', opts)
 
--- insert closed brace
+-- insert closing brace
 keymap('i', '(', '()<Left>', opts)
 keymap('i', '[', '[]<Left>', opts)
 keymap('i', '{', '{}<Left>', opts)
+keymap('i', '"', '""<Left>', opts)
+keymap('i', "'", "''<Left>", opts)
 
 -- normal mode in terminal mode
-keymap('t', '<C-w>h', '<C-\\><C-n>', opts)
+keymap('t', '<C-c>', '<C-\\><C-n>', opts)
+keymap('t', '<Esc>', '<C-\\><C-n>', opts)
+
+-- surround selected text with bracktes
+vim.surround_with = function(start_char, end_char)
+    local line_start = vim.fn.getpos("'<")[2]
+    local line_end = vim.fn.getpos("'>")[2]
+    local col_start = vim.fn.getpos("'<")[3]
+    local col_end = vim.fn.getpos("'>")[3]
+
+    -- adjust column end if in visual line mode
+    if vim.fn.visualmode() == "V" then
+        col_end = #vim.fn.getline(line_end)
+    end
+
+    -- if it's a multi-line selection
+    if line_start ~= line_end then
+        vim.cmd(string.format('call setline(%d, "%s" . getline(%d))', line_start, start_char, line_start))
+        vim.cmd(string.format('call setline(%d, getline(%d) . "%s")', line_end, line_end, end_char))
+    else
+        -- for single line selection
+        local line_content = vim.fn.getline(line_start)
+        local before = line_content:sub(1, col_start - 1)
+        local middle = line_content:sub(col_start, col_end)
+        local after = line_content:sub(col_end + 1)
+        vim.cmd(string.format('call setline(%d, "%s%s%s%s%s")', line_start, before, start_char, middle, end_char, after))
+    end
+
+    -- move the cursor to the closing bracket
+    vim.api.nvim_win_set_cursor(0, {line_end, col_end + 1})
+end
+
+keymap('v', '<leader>(', ':lua vim.surround_with("(", ")")<CR>', opts)
+keymap('v', '<leader>{', ':lua vim.surround_with("{", "}")<CR>', opts)
+keymap('v', '<leader>[', ':lua vim.surround_with("[", "]")<CR>', opts)
+keymap('v', '<leader><', ':lua vim.surround_with("<", ">")<CR>', opts)
+
--- plugin/vim-airline.lua
+++ plugin/vim-airline.lua
@@ -1,9 +1,12 @@
 vim.cmd [[
     let g:airline#extensions#nvimlsp#enabled = 1
 
-    let g:airline_theme = 'luna'
+    let g:airline_theme = 'powerlineish'
 
     let g:airline#extensions#tabline#enabled = 1
+    let g:airline#extensions#tabline#formatter = 'unique_tail'
+    let g:airline#extensions#tabline#left_sep = ' '
+    let g:airline#extensions#tabline#left_alt_sep = '|'
 
     let g:airline#extensions#nvimlsp#error_symbol = ' '
     let g:airline#extensions#nvimlsp#warning_symbol = ' '
